447cdc764b8a431023770146be10a310f49c8421
hph <kjustdoitno1@gmail.com>
2026-01-12 08:18:42 +0000

    pika umi ee2ee train cfg

---
+++ src/lerobot/datasets/hirol/utils/trim.sh
@@ -0,0 +1,3 @@
+#!bin/bash
+
+python src/lerobot/datasets/hirol/utils/trim_static_frames.py -i /data/2025/unitree_co-train/bread_picking/1227_duo_unitree_bread_picking_human_182ep -o /data/2025/unitree_co-train/bread_picking/trimmed/1227_duo_unitree_bread_picking_human_182ep
